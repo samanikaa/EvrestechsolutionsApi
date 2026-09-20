3f286850fddc08254011fe53b1ab8a3c4ab70a1b
Andrew Samanika <samanikaa@praz.org.zw>
2026-09-20 13:11:41 +0200

    Patch for expectsJSon() to return null or the registered login route

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->redirectGuestsTo(
+            fn(Request $request)=> $request->expectsJson() ? null : '/login'
+        );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
